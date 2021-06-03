acdb5b67d7161c4b874d6551ab0379592a95eaff
Kevin Barraza Sandoval <54358995+KevBS@users.noreply.github.com>
2021-06-02 21:57:12 -0500

    Add files via upload

---
+++ Proyecto Final/js/crud-constancias.js
@@ -0,0 +1,18 @@
+/*Conrolador*/
+alert("Hola");
+function actionCreate() {
+
+    alert("Crear un nuevo registro")
+}
+
+
+function actionRead() {
+    alert("Leer un nuevo registro")
+}
+
+function actionUpdate() {
+    alert("Actualizar un nuevo registro")
+}
+function actionDelete() {
+    alert("Eliminar un nuevo registro")
+}
---
+++ Proyecto Final/php/crud-denominacion.php
@@ -0,0 +1,7 @@
+<?php
+    /*Modelo de datos = Modelo-Vista-Controlador
+        Modelo          = .PHP 
+        Vista           = .HTML 
+        Controlador     = .JS
+    */
+?>
